60232d31e0d08af322df37ccfd85cf844e8e3fc8
BryanGIG <contacts.bryangig@gmail.com>
2023-01-14 08:54:57 +0700

    [New-Fix] Optimize, Fix Item, etc
    - [Optimize] scrapping time
    - [Fix] misplaces item in different category
    - [New] Download image from server url `IconHD`
    - [New] Resize image to 60px `Icon`

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const sharp = require('sharp');
+
+const readFileAsync = (fileName) => {
+   return new Promise((resolve, reject) => {
+      fs.readFile(fileName, (err, data) => {
+         if (err)
+            reject(err);
+         resolve(data);
+      });
+   });
+}
 
 const dataToJsonFile = (data) => {
    fs.writeFile("data.json", data, "binary", (err) => {
@@ -9,90 +20,142 @@ const dataToJsonFile = (data) => {
    });
 }
 
-const getCategory = () => {
+const getWeaponDatas = () => {
    return new Promise((resolve, reject) => {
-      axios.get("https://main.codm.garena.co.id/guide/weapons/assault_rifle")
+      axios.get(`https://main.codm.garena.co.id/guide/weapons/assault_rifle`)
          .then(req => {
-            const category = [];
-            const $ = cheerio.load(req.data);
-            const weaponLayout = $('div[id="WeaponLayout"]');
-            weaponLayout.find("ul li").each(function (_, e) {
-               category.push($(e).text());
-            });
-
-            if (category != undefined) {
-               resolve(category);
-            } else {
-               reject("Empty data");
-            }
-         });
-   });
-}
-
-const getWeapon = (weaponCategory, weaponType) => {
-   return new Promise((resolve, reject) => {
-      axios.get(`https://main.codm.garena.co.id/guide/weapons/${weaponType}`)
-         .then(req => {
-            const title = [];
-            const img = [];
-
             const data = {};
-            data[weaponCategory] = [];
 
             const $ = cheerio.load(req.data);
-            const weaponList = $("div.weapon-list__list");
-            weaponList.each(function (i, e) {
-               title.push($(e).find("div.name-cont").text());
-               img.push($(e).find("div.image-cont").find("img").attr('src'));
-            });
-            if (title == undefined || img == undefined) {
-               reject("Empty Data!");
-            } else {
 
-               var i;
-               for (i = 0; i < title.length; i++) {
+            const weaponCategoryList = $("div.tabs__content div.tab-container__content");
+
+            weaponCategoryList.each(function (_, category) {
+               const title = [];
+               const img = [];
+
+               const tCategory = $(category).find("span.title").text();
+
+               data[tCategory] = [];
+
+               const weaponList = $(category).find("div.weapon-list div.weapon-list__list div.left");
+
+               weaponList.find("div.name-cont").each(function (_, e) {
+                  title.push($(e).text());
+               })
+
+               weaponList.find("div.image-cont img").each(function (_, e) {
+                  img.push($(e).attr('src'))
+               })
+
+               if (title == undefined || img == undefined) {
+                  reject("Error occured title or image");
+               }
+
+               for (let index = 0; index < title.length; index++) {
                   let isi = {
-                     "title": title[i],
-                     "img": img[i]
+                     "title": title[index],
+                     "img": img[index]
                   }
-                  data[weaponCategory].push(isi);
-               }
 
-               if (data == undefined) {
-                  reject("No result :(");
-               } else {
-                  resolve(data);
+                  data[tCategory].push(isi);
                }
+            });
 
-            };
+            if (data == undefined) {
+               reject("No result :(");
+            } else {
+               resolve(data);
+            }
+
          });
    });
 };
 
-function jsonConcat(o1, o2) {
-   for (var key in o2) {
-      o1[key] = o2[key];
-   }
-   return o1;
+async function fetchServerData() {
+   const output = await getWeaponDatas();
+
+   var result = JSON.stringify(output, null, 2);
+
+   dataToJsonFile(result);
 }
 
-async function main() {
-   const data = await getCategory();
-   var output = {};
+async function saveImgFromJsonFile() {
+   const rawData = await readFileAsync("data.json");
+   const data = JSON.parse(rawData.toString());
 
    for (const key in data) {
       if (Object.hasOwnProperty.call(data, key)) {
-         const element = data[key];
+         const arrData = data[key];
 
-         const category = element.replace(" ", "_").toLowerCase();
-         console.info("Weapon Category : ", element);
-         const weapon = await getWeapon(element, category);
-         output = jsonConcat(output, weapon);
+         for (const jWeapon in arrData) {
+            if (Object.hasOwnProperty.call(arrData, jWeapon)) {
+               const element = arrData[jWeapon];
+               const imageRaw = await axios.get(element["img"], { responseType: 'arraybuffer' });
+
+               const imageExtensionRaw = element["img"].split(".");
+               const imageExtension = imageExtensionRaw[imageExtensionRaw.length - 1];
+
+               const imageNameRaw = element["title"].replaceAll(" ", "-");
+               const imageName = imageNameRaw + "." + imageExtension;
+
+               fs.mkdir("IconHD", (err) => { });
+               fs.writeFile(`IconHD/${imageName}`, imageRaw.data, "binary", (err) => {
+                  if (err) throw err;
+                  console.log("Download: ", imageName);
+               });
+            }
+         }
       }
    }
+}
 
-   var result = JSON.stringify(output, null, 2);
-   dataToJsonFile(result);
+async function resizeDownloadImage() {
+   const rawData = await readFileAsync("data.json");
+   const data = JSON.parse(rawData.toString());
+
+   for (const key in data) {
+      if (Object.hasOwnProperty.call(data, key)) {
+         const arrData = data[key];
+
+         for (const jWeapon in arrData) {
+            const element = arrData[jWeapon];
+
+            const imageExtensionRaw = element["img"].split(".");
+            const imageExtension = imageExtensionRaw[imageExtensionRaw.length - 1];
+
+            const imageNameRaw = element["title"].replaceAll(" ", "-");
+            const imageName = imageNameRaw + "." + imageExtension;
+
+            fs.mkdir("Icon", (err) => { });
+            sharp(`IconHD/${imageName}`)
+               .resize(60)
+               .toFile(`Icon/${imageName}`, (err, info) => {
+                  if (err)
+                     console.error(err);
+                  else
+                     console.log("Resize: ", imageName);
+               })
+         }
+      }
+   }
+}
+
+/**
+ * If you want to fetch data from server use :
+ * main();
+ * 
+ * If you want to save image data from server use :
+ * saveImgFromJsonFile();
+ * 
+ * If you want to resize all the image use :
+ * resizeDownloadImage();
+ * 
+ */
+async function main() {
+   await fetchServerData();
+   await saveImgFromJsonFile();
+   await resizeDownloadImage();
 }
 
 main();
